1bc35e52e4f3b46e6666bb6c4c5d8a3609917d03
Hunter <synthia@ipayafrica.com>
2023-01-05 11:10:35 +0300

    implement email

--- functions/employer/index.js
+++ functions/employer/index.js
@@ -7,6 +7,7 @@ import login from "./routes/login.js";
 import allEmployees from "./routes/getAllemployees.js";
 import {authenticateToken} from "../libraries.js";
 import addTask from "./routes/createTask.js";
+import createEmployee from "./routes/createEmployee.js";
 
 const employer = express().use(cors({origin: true}));
 employer.post("/", async (req, res) => {
@@ -25,6 +26,7 @@ employer.post("/createEmployer", createEmployer);
 employer.post("/loginEmployer", login);
 employer.get("/all-employees", authenticateToken, allEmployees);
 employer.post("/add-task", authenticateToken, addTask);
+employer.post("/create-employee", authenticateToken, createEmployee);
 
 export default
 functions.region("europe-west3").https.onRequest(employer);

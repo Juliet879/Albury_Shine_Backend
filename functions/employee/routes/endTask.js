1c7299120fe22eea70174fb2eae6b400d0b7d06c
Hunter <achienghunter@gmail.com>
2023-02-01 08:02:21 +0300

    implement end task

--- functions/employee/routes/endTask.js
+++ functions/employee/routes/endTask.js
@@ -27,29 +27,22 @@ const endTask = async (req, res)=>{
     const getAssigneeProgress = getTask.data().data
         .filter((item)=>item.assigneeId === assigneeId);
     console.log(getAssigneeProgress);
-    const data = {
-      startTime: getAssigneeProgress[0].startTime,
-      location: location,
-      assigneeId: assigneeId,
-      status: "Completed",
-      endTime: endTime,
-    };
 
 
-    // const checkUser = getTask.data().data;
-    // const check = checkUser.filter((item)=>item. === assigneeId);
-    // if (check.length !== 0) {
-    //   res.status(409)
-    //       .send({
-    //         status: 409,
-    //         success: false,
-    //         error: "User cannot end the same task twice",
-    //       });
-    // }
+    const checkUser = getTask.data().data;
+    const data = checkUser.map((item)=>{
+      if (item.assigneeId === assigneeId) {
+        item.location = location;
+        item.status="Completed";
+        item.endTime= endTime;
+      }
+      return item;
+    });
+
     await db.collection("task-progress")
         .doc(taskId)
         .update({
-          data: FieldValue.arrayUnion(data),
+          data: data,
         });
 
 
@@ -63,6 +56,7 @@ const endTask = async (req, res)=>{
       message: "Task ended and updated successfully",
     });
   } catch (error) {
+    console.log(error);
     res.status(500).send({
       status: 500,
       success: false,
